441c602d5001162673ffda19cbe30bbf5b9286f0
miguelzvd <dilsonmiguel25@gmail.com>
2025-08-02 16:34:33 -0300

    Update package.json to include tailwind-merge dependency; refactor ProjectCard component for better structure and reusability, enhancing project display on the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,7 +109,7 @@ export default function Home() {
                   <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
                     <Image
                       className="object-contain invert"
-                      src={"/images/Stacks/next.png"}
+                      src={"/images/stacks/next.png"}
                       alt={"Next Logo"}
                       width={100}
                       height={100}
@@ -119,7 +119,7 @@ export default function Home() {
                   <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
                     <Image
                       className="object-contain"
-                      src={"/images/Stacks/chakra.png"}
+                      src={"/images/stacks/chakra.png"}
                       alt={"Chakra Logo"}
                       width={100}
                       height={100}
@@ -129,7 +129,7 @@ export default function Home() {
                   <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
                     <Image
                       className="object-contain"
-                      src={"/images/Stacks/reactjs.png"}
+                      src={"/images/stacks/reactjs.png"}
                       alt={"Next Logo"}
                       width={100}
                       height={100}
@@ -139,7 +139,7 @@ export default function Home() {
                   <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
                     <Image
                       className="object-contain w-5"
-                      src={"/images/Stacks/django.png"}
+                      src={"/images/stacks/django.png"}
                       alt={"Next Logo"}
                       width={100}
                       height={100}
@@ -150,11 +150,376 @@ export default function Home() {
 
               <div className="flex flex-col h-full gap-0">
                 <span>Project title</span>
-                <div className="w-full h-full bg-white/20 rounded-md p-1">
+                <div className="w-full h-full bg-white/20 rounded-md overflow-hidden">
                   <Image
-                    className="object-contain"
-                    src={""}
-                    alt={"Next Logo"}
+                    className="cover h-full w-full"
+                    src={"/images/projects/jovem_potiguar.png"}
+                    alt={"Jovem Potiguar Project Photo"}
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-row gap-1 flex-wrap">
+                <a
+                  href=""
+                  className="border border-gray-300 rounded-sm w-fit flex flex-row justify-start items-center py-0 pl-1 pr-2"
+                >
+                  <Link height={"1rem"} />
+                  <span className="text-sm">Link</span>
+                </a>
+                <button className="border cursor-pointer border-gray-300 rounded-sm py-0 px-2 justify-start w-fit">
+                  <span className="text-sm">+ More about the project</span>
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4 h-80 w-70 bg-dark-gray p-4 rounded-md">
+              <div className="flex flex-col gap-1">
+                <span>Stack</span>
+                <div className="flex flex-row gap-1 flex-wrap">
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain invert"
+                      src={"/images/stacks/next.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain"
+                      src={"/images/stacks/chakra.png"}
+                      alt={"Chakra Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain"
+                      src={"/images/stacks/reactjs.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain w-5"
+                      src={"/images/stacks/django.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col h-full gap-0">
+                <span>Project title</span>
+                <div className="w-full h-full bg-white/20 rounded-md overflow-hidden">
+                  <Image
+                    className="cover h-full w-full"
+                    src={"/images/projects/jovem_potiguar.png"}
+                    alt={"Jovem Potiguar Project Photo"}
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-row gap-1 flex-wrap">
+                <a
+                  href=""
+                  className="border border-gray-300 rounded-sm w-fit flex flex-row justify-start items-center py-0 pl-1 pr-2"
+                >
+                  <Link height={"1rem"} />
+                  <span className="text-sm">Link</span>
+                </a>
+                <button className="border cursor-pointer border-gray-300 rounded-sm py-0 px-2 justify-start w-fit">
+                  <span className="text-sm">+ More about the project</span>
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4 h-80 w-70 bg-dark-gray p-4 rounded-md">
+              <div className="flex flex-col gap-1">
+                <span>Stack</span>
+                <div className="flex flex-row gap-1 flex-wrap">
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain invert"
+                      src={"/images/stacks/next.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain"
+                      src={"/images/stacks/chakra.png"}
+                      alt={"Chakra Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain"
+                      src={"/images/stacks/reactjs.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain w-5"
+                      src={"/images/stacks/django.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col h-full gap-0">
+                <span>Project title</span>
+                <div className="w-full h-full bg-white/20 rounded-md overflow-hidden">
+                  <Image
+                    className="cover h-full w-full"
+                    src={"/images/projects/jovem_potiguar.png"}
+                    alt={"Jovem Potiguar Project Photo"}
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-row gap-1 flex-wrap">
+                <a
+                  href=""
+                  className="border border-gray-300 rounded-sm w-fit flex flex-row justify-start items-center py-0 pl-1 pr-2"
+                >
+                  <Link height={"1rem"} />
+                  <span className="text-sm">Link</span>
+                </a>
+                <button className="border cursor-pointer border-gray-300 rounded-sm py-0 px-2 justify-start w-fit">
+                  <span className="text-sm">+ More about the project</span>
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4 h-80 w-70 bg-dark-gray p-4 rounded-md">
+              <div className="flex flex-col gap-1">
+                <span>Stack</span>
+                <div className="flex flex-row gap-1 flex-wrap">
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain invert"
+                      src={"/images/stacks/next.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain"
+                      src={"/images/stacks/chakra.png"}
+                      alt={"Chakra Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain"
+                      src={"/images/stacks/reactjs.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain w-5"
+                      src={"/images/stacks/django.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col h-full gap-0">
+                <span>Project title</span>
+                <div className="w-full h-full bg-white/20 rounded-md overflow-hidden">
+                  <Image
+                    className="cover h-full w-full"
+                    src={"/images/projects/jovem_potiguar.png"}
+                    alt={"Jovem Potiguar Project Photo"}
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-row gap-1 flex-wrap">
+                <a
+                  href=""
+                  className="border border-gray-300 rounded-sm w-fit flex flex-row justify-start items-center py-0 pl-1 pr-2"
+                >
+                  <Link height={"1rem"} />
+                  <span className="text-sm">Link</span>
+                </a>
+                <button className="border cursor-pointer border-gray-300 rounded-sm py-0 px-2 justify-start w-fit">
+                  <span className="text-sm">+ More about the project</span>
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4 h-80 w-70 bg-dark-gray p-4 rounded-md">
+              <div className="flex flex-col gap-1">
+                <span>Stack</span>
+                <div className="flex flex-row gap-1 flex-wrap">
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain invert"
+                      src={"/images/stacks/next.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain"
+                      src={"/images/stacks/chakra.png"}
+                      alt={"Chakra Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain"
+                      src={"/images/stacks/reactjs.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain w-5"
+                      src={"/images/stacks/django.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col h-full gap-0">
+                <span>Project title</span>
+                <div className="w-full h-full bg-white/20 rounded-md overflow-hidden">
+                  <Image
+                    className="cover h-full w-full"
+                    src={"/images/projects/jovem_potiguar.png"}
+                    alt={"Jovem Potiguar Project Photo"}
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-row gap-1 flex-wrap">
+                <a
+                  href=""
+                  className="border border-gray-300 rounded-sm w-fit flex flex-row justify-start items-center py-0 pl-1 pr-2"
+                >
+                  <Link height={"1rem"} />
+                  <span className="text-sm">Link</span>
+                </a>
+                <button className="border cursor-pointer border-gray-300 rounded-sm py-0 px-2 justify-start w-fit">
+                  <span className="text-sm">+ More about the project</span>
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4 h-80 w-70 bg-dark-gray p-4 rounded-md">
+              <div className="flex flex-col gap-1">
+                <span>Stack</span>
+                <div className="flex flex-row gap-1 flex-wrap">
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain invert"
+                      src={"/images/stacks/next.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain"
+                      src={"/images/stacks/chakra.png"}
+                      alt={"Chakra Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain"
+                      src={"/images/stacks/reactjs.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+
+                  <div className="w-8 h-8 flex flex-col justify-center items-center bg-white rounded-md p-1">
+                    <Image
+                      className="object-contain w-5"
+                      src={"/images/stacks/django.png"}
+                      alt={"Next Logo"}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col h-full gap-0">
+                <span>Project title</span>
+                <div className="w-full h-full bg-white/20 rounded-md overflow-hidden">
+                  <Image
+                    className="cover h-full w-full"
+                    src={"/images/projects/jovem_potiguar.png"}
+                    alt={"Jovem Potiguar Project Photo"}
                     width={100}
                     height={100}
                   />
