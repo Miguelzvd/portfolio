51d50265ea8a5f4b2bb21fa86b1fc33e843d6319
miguelzvd <dilsonmiguel25@gmail.com>
2025-08-11 13:34:46 -0300

    refactor(ui): improve StackCardsSection layout for better spacing
    - Increase vertical gap between grid rows in StackCardsSection for enhanced responsiveness on medium and large screens.

--- src/components/ui/StackCardsSection.tsx
+++ src/components/ui/StackCardsSection.tsx
@@ -54,7 +54,7 @@ const stacks = [
 
 export const StackCardsSection = () => {
   return (
-    <Section.Content className="grid grid-flow-row grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-10 xl:gap-x-20 gap-y-8 max-h-fit">
+    <Section.Content className="grid grid-flow-row grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-10 xl:gap-x-20 gap-y-8 md:gap-y-30 lg:gap-y-46 max-h-fit">
       {stacks.map(({ src, alt, description, shadow }, index) => (
         <StackCard.Root key={index}>
           <StackCard.Image src={src} alt={alt} unoptimized />
